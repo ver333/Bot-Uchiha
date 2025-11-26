60ba37a192240ce83cb7a10e56abe03f288f85d7
kks115 <kksadas69@gmail.com>
2025-11-26 20:02:00 +0100

    Create centralized configuration file

---
+++ config/settings.js
@@ -0,0 +1,9 @@
+// Centralized configuration for the Bot-Uchiha
+
+const config = {
+    version: '1.0.0',
+    apiEndpoint: 'https://api.example.com',
+    defaultLanguage: 'en',
+};
+
+module.exports = config;
