f2f3f72a961add94c7aaaf421a5b762f36a29567
Rúben Silver <drogadinhosilver@gmail.com>
2025-12-02 12:14:31 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,34 @@
-import express from "express";
+import express, { Request, Response } from "express";
 import dotenv from "dotenv";
+
 dotenv.config();
 
 const app = express();
 app.use(express.json());
 
-const VERIFY_TOKEN = process.env.VERIFY_TOKEN;
+const VERIFY_TOKEN = process.env.VERIFY_TOKEN || "UCHIHA_VERIFIED";
 
-app.get("/webhook", (req, res) => {
+// GET (verificação do webhook)
+app.get("/webhook", (req: Request, res: Response) => {
   const mode = req.query["hub.mode"];
   const token = req.query["hub.verify_token"];
   const challenge = req.query["hub.challenge"];
 
   if (mode && token && mode === "subscribe" && token === VERIFY_TOKEN) {
-    console.log("Webhook verificado com sucesso!");
+    console.log("WEBHOOK VERIFICADO COM SUCESSO!");
     res.status(200).send(challenge);
   } else {
     res.sendStatus(403);
   }
 });
 
-app.post("/webhook", (req, res) => {
-  console.log("Mensagem recebida:", JSON.stringify(req.body, null, 2));
+// POST (mensagens recebidas)
+app.post("/webhook", (req: Request, res: Response) => {
+  console.log("Recebido:", JSON.stringify(req.body, null, 2));
   res.sendStatus(200);
 });
 
-app.listen(process.env.PORT || 3000, () => {
-  console.log("🚀 Servidor rodando...");
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Servidor rodando na porta ${port}`);
 });
